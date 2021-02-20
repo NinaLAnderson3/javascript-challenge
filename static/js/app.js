9d9dd34d097c8e21eff3c62e5e773ed7ec04db31
Nina Anderson <ninamonaco@yahoo.com>
2021-02-20 16:09:46 -0500

    updated code

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,9 @@ console.log(data);
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
+var button =d3.select("#filter-btn")
+var inputDate = d3.select("#datetime")
+var inputCity = d3.select("#city")
 console.log(data);
 
 data.forEach(function(logdata){
@@ -24,15 +27,6 @@ data.forEach(function(logdata){
     });
 });
 
-data.forEach(function(logdata){
-    console.log(logdata);
-    var row = tbody.append("tr");
-    Object.entries(logdata).forEach(function([key,value]){
-        console.log(key, value);
-
-        var cell = row.append("td");
-    });
-});
 
 data.forEach(function(logdata){
     console.log(logdata);
@@ -44,12 +38,35 @@ data.forEach(function(logdata){
         cell.text(value);
     });
 });
-// var table = d3.select("ufo-table");
 
-// table.attr("class","table table-striped")
+// from data.js
+var tableData = data;
+console.log(tableData);
 
-// var tbody = d3.select("tbody")
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-// var row = tbody.append("tr");
-// row.append("td").text(tableData);
 
+
+var button = d3.select("#filter-btn");
+button.on("click",function()
+{
+    tbody.html("");
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(sighting=>sighting.datetime === inputValue);
+    console.log(filteredData)
+
+    filteredData.forEach(function(selections){
+        console.log(selections);
+        var row =tbody.append("tr");
+        Object.entries(selections).forEach(function([key, value]){
+            console.log(key, value);
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
